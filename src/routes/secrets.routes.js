38a3634da8e8b2cba833f295d10f7ecd2c5ec9e2
Jesse Krensel <jdkrensel@gmail.com>
2023-01-12 09:52:35 -0500

    add passport config

--- src/routes/secrets.routes.js
+++ src/routes/secrets.routes.js
@@ -1,14 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport")
+const session = require("express-session");
+const passportLocalMongoose = require("passport-local-mongoose");
+const User = require("../models/user");
+const checkAuthenticated = require("../middleware/checkAuthenticated")
+
+router.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+router.use(passport.initialize());
+router.use(passport.session());
+
+// configure passport-local-mongoose
+passport.use(User.createStrategy());
+
+// configure sessions
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture
+    });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
 
 router
   .route("/")
-  .get((req, res) => {
-    if (req.isAuthenticated()) {
+  .get(checkAuthenticated, (req, res) => {
       res.render("secrets");
-    } else {
-      res.redirect("/login");
-    }
   });
 
 module.exports = router;
