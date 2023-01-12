3fcbede26d13c92ccc2f305c1bd6f9207f1ccd0a
Jesse Krensel <jdkrensel@gmail.com>
2023-01-12 09:52:27 -0500

    add passport config

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,6 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const User = require("../models/user");
+const passport = require("passport")
+const session = require("express-session");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+router.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(passport.initialize());
+router.use(passport.session());
+
+// configure passport-local-mongoose
+passport.use(User.createStrategy());
+
+// configure sessions
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture
+    });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
 
 router
   .route("/")
@@ -12,6 +47,17 @@ router
       email: req.body.username,
       password: req.body.password,
     });
+
+    req.login(user, (err) =>{
+      if (err) {
+        console.log(err);
+      } else {
+        passport.authenticate("local")(req, res, () =>{
+          res.redirect("/secrets");
+        });
+      }
+    });
+  
   });
 
 module.exports = router;
